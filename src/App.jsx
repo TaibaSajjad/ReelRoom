d22293a17614cfd7a7456329f50e55c352b9570c
Taiba Sajjad <taibasajjad08@gmail.com>
2026-09-12 01:31:03 -0700

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/ReelRoom">
 
       {/* FULL PAGE BACKGROUND */}
       <div className="page-bg">
